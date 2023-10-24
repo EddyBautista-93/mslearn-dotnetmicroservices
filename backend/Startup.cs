4fdb2ed0d2a2815b6fcc4df166c0820d7eb58bbd
EddyBautista <bautista.j.eddy@gmail.com>
2023-10-23 19:26:22 -0500

    remove swagger

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -26,7 +26,7 @@ namespace backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen();
+            // services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,8 +37,8 @@ namespace backend
                 app.UseDeveloperExceptionPage();
 
             }
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backend v1"));
+            // app.UseSwagger();
+            // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backend v1"));
 
             app.UseRouting();
 
